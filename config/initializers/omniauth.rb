f4b6699877880814d4af7c494f38b1d3adec8cdf
都是牛奶装什么特仑苏丶 <simlegate@163.com>
2013-10-06 10:56:24 +0800

    隐藏GITHUB_SECRET and GITHUB_KEY在本地 [fix #1]

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,3 @@
-GITHUB_KEY = '2b353c54337c0cb1418d'
-
-GITHUB_SECRET =  'b2e25edee22b2cf540b61864f7ca745bfa26cae6'
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, GITHUB_KEY, GITHUB_SECRET
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
